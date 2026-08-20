888b3db85e89a447a844d19f907b48a7b50a8630
vitorhugo-dotnet <hugodigital12@gmail.com>
2026-08-20 12:57:14 -0300

    chore: scaffold do reator maven com orders-api e payments-api

---
+++ orders-api/src/test/java/com/vitorhugo/orders/OrdersApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.vitorhugo.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class OrdersApiApplicationTests {
+
+    @Test
+    void contextoDeveSubir() {
+    }
+}
