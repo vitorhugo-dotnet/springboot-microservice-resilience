0af61423e3c51a1fa274df01ad379bcde548c464
vitorhugo-dotnet <hugodigital12@gmail.com>
2026-09-12 14:24:52 -0300

    feat: add demo ui baseline module

---
+++ demo-ui/src/main/java/com/vitorhugo/demoui/DemoUiApplication.java
@@ -0,0 +1,12 @@
+package com.vitorhugo.demoui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoUiApplication.class, args);
+    }
+}
